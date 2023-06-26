2ca0fed02cff5ac141891c1be35970061a5315ae
wangkailang <wangkailang@vikadata.com>
2023-06-26 14:55:56 +0800

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { initializeWidget } from '@apitable/widget-sdk';
+import { initializeWidget, useActiveCell, useRecord } from '@apitable/widget-sdk';
 import { Setting } from './setting';
+import { colorVars, Button } from '@apitable/components';
+
 
 export const HelloWorld = () => {
-  return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <div style={{ flexGrow: 1, overflow: 'auto', padding: '0 8px'}}>
-        hello，world 🚀
-      </div>
-      <Setting />
-    </div>
-  );
+    const activeCell = useActiveCell();
+    const activeRecord = useRecord(activeCell?.recordId);
+    if (!activeCell || !activeRecord) {
+        return <p>无激活的单元格</p>
+    }
+
+    return (
+        <div style={{ display: 'flex', height: '100%' }}>
+          <div style={{ flexGrow: 1, overflow: 'auto', padding: '0 8px'}}>
+            hello，world 🚀
+              <p style={{ color: colorVars.textCommonPrimary }}>激活的单元格: {activeRecord.getCellValueString(activeCell.fieldId)}</p>
+              <Button color="primary">点击</Button>
+          </div>
+          <Setting />
+        </div>
+    );
 };
 
 
